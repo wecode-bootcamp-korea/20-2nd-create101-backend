77a2a4bf16c6553d8183dbd55eeabecd1dd21d1d
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-25 10:48:06 +0900

    ADD: models.py in courses, users app
    - users app 추가
    - courses app 추가
    - app 마다 models.py 작성
    - makemigrate 실행

---
+++ users/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+class User(models.Model):
+    email       = models.CharField(max_length=128)
+    korean_name = models.CharField(max_length=64)
+    password    = models.CharField(max_length=64)
+    first_login = models.BooleanField(default=True)
+    coupon      = models.ManyToManyField('Coupon', through='UserCoupon', related_name='users')
+    comment     = models.ManyToManyField('courses.Review', through='Comment', related_name='reviewer')
+    like        = models.ManyToManyField('courses.Course', through='Like', related_name='liked_user')
+    look        = models.ManyToManyField('courses.Course', through='Look', related_name='looked_user')
+
+    class Meta:
+        db_table = 'users'
+
+class Coupon(models.Model):
+    name          = models.CharField(max_length=128)
+    discount_rate = models.IntegerField()
+
+    class Meta:
+        db_table = 'coupons'
+
+class UserCoupon(models.Model):
+    coupon = models.ForeignKey('Coupon', on_delete=models.CASCADE)
+    user   = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'user_coupon'
+
+class Comment(models.Model):
+    review = models.ForeignKey('courses.Review', on_delete=models.CASCADE)
+    user   = models.ForeignKey('User', on_delete=models.CASCADE, related_name='comments')
+
+    class Meta:
+        db_table = 'comments'
+
+class Like(models.Model):
+    course = models.ForeignKey('courses.Course', on_delete=models.CASCADE)
+    user   = models.ForeignKey('User', on_delete=models.CASCADE, related_name='likes')
+
+    class Meta:
+        db_table = 'likes'
+
+class Look(models.Model): 
+    course = models.ForeignKey('courses.Course', on_delete=models.CASCADE)
+    user   = models.ForeignKey('User', on_delete=models.CASCADE, related_name='looks')
+
+    class Meta:
+        db_table = 'looks'
