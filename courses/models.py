cb70cc559272fb82da5ddfbdc6bb894a17b4774c
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-25 11:03:26 +0900

    CREATE: models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,42 @@
-from django.db import models
+from django.db                 import models
 
-# Create your models here.
+class Course(models.Model):
+    target       = models.ForeignKey('Target', on_delete=models.CASCADE)
+    description  = models.TextField()
+    thumbnail    = models.CharField(max_length=2000)
+    price        = models.DecimalField(max_digits=8, decimal_places=2)
+    title        = models.CharField(max_length=300)
+    sub_category = models.ForeignKey('SubCategory', on_delete=models.CASCADE)
+    user         = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    month        = models.IntegerField()
+    created_at   = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'courses'
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        db_table = 'categories'
+
+class SubCategory(models.Model):
+    name     = models.CharField(max_length=64)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='sub_categorys')
+
+    class Meta:
+        db_table='sub_categories'
+
+class Target(models.Model):
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        db_table = 'targets'
+
+class Review(models.Model):
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    user   = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    text   = models.TextField()
+
+    class Meta:
+        db_table = 'reviews'
