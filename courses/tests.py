65ad3b79f253caac142c7c4ba9e1420431c1c632
ChoiWooseok <diddmlvkf@gmail.com>
2021-05-26 13:37:38 +0900

    ADD: category view 추가
    - api url 구현
    - CategoryView 구현
    - Unit test 구현

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from courses.models import Category, SubCategory
 
-# Create your tests here.
+class CategoryTest(TestCase):
+    def setUp(self):
+        category = Category.objects.create(
+                id   = 1,
+                name = 'test_category1'
+            )
+        
+        SubCategory.objects.create(
+            id       = 1,
+            name     = 'test_sub_category1',
+            category = category
+        )
+        SubCategory.objects.create(
+            id       = 2,
+            name     = 'test_sub_category2',
+            category = category
+        )
+        
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        SubCategory.objects.all().delete()
+       
+    def test_category_get_view(self):
+        client   = Client()
+        response = client.get('/courses/category')
+        self.assertEqual(response.json(), {
+            'category': [
+                {
+                    'id'          : 1,
+                    'name'        : 'test_category1',
+                    'sub_category': [
+                            {
+                            'id'  : 1,
+                            'name': 'test_sub_category1'
+                            },
+                            {
+                            'id'  : 2,
+                            'name': 'test_sub_category2'
+                            }
+                        ]
+                }
+            ]
+        })
+        self.assertEqual(response.status_code, 200)
+
+
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views import View
+from django.http  import JsonResponse
 
-# Create your views here.
+from courses.models import Category
+
+class CategoryView(View):
+    def get(self, request):
+        results = [{
+                'id'          : category.id,
+                'name'        : category.name,
+                'sub_category': [{
+                    'id'  : sub_category.id,
+                    'name': sub_category.name
+                    }for sub_category in category.sub_categorys.all()]
+             }for category in Category.objects.all()]
+        return JsonResponse({'category': results}, status=200)
