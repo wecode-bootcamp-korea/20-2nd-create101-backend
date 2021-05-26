5de9d24b4b56d173a6bb0e202bac8997e9dc813f
seungho shin <shinisgood@seunghoui-MacBookAir.local>
2021-05-26 16:19:23 +0900

    카카오 소셜 로그인 - 사용자정보 받아오기 구현 - 첫 로그인 한사람에게 쿠폰 증정 - 유닛테스트 구현 - (refactor) 주석 삭제 및 유닛테스트 수정

--- create101/urls.py
+++ create101/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('courses', include('courses.urls'))
+    path('courses', include('courses.urls')),
+    path('users', include('users.urls')),
 ]
