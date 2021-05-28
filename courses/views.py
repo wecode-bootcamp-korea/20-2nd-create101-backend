4ffc78ec4606f09caf28090c1c9046bb20ab0842
jeonhs2482 <66204827+jeonhs2482@users.noreply.github.com>
2021-05-28 19:58:35 +0900

    MODIFY: Pull Request
    -Modify CourseDetailView Pull Request
    -Modify CourseDetailView Test Code Pull Request
    -Delete loads_data.py

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,8 @@
-from django.views import View
-from django.http  import JsonResponse
+from django.http.response   import JsonResponse
+from django.views           import View
 
-from courses.models import Category
+from courses.models         import Course, Review, Category
+from users.models           import User, Like, Comment
 
 class CategoryView(View):
     def get(self, request):
@@ -14,3 +15,35 @@ class CategoryView(View):
                     }for sub_category in category.sub_categorys.all()]
              }for category in Category.objects.all()]
         return JsonResponse({'category': results}, status=200)
+
+class CourseDetailView(View):
+    def get(self, request, id):
+        try:
+            course          = Course.objects.get(id=id)
+            course_like     = Like.objects.filter(course_id=id)
+            course_review   = Review.objects.filter(course_id=id)
+            comments        = [question_id.id for question_id in course_review]
+            review_comments = Comment.objects.filter(review_id__in=comments)
+            course_info = {
+                "id"            : course.id,
+                "name"          : course.title,
+                "price"         : course.price,
+                "thumbnail_url" : course.thumbnail,
+                "subcategory"   : course.sub_category.name,
+                "counts_like"   : course_like.count(),
+                "target"        : course.target.name,
+                "month"         : course.month,
+                "review"        : [{
+                    'id'     : review.id,
+                    'text'   : review.text,
+                    'user_id': review.user_id
+                } for review in course_review],
+                "comment"       : [{
+                    'text'       : comment.text,
+                    'user_id'  : comment.user_id,
+                    'review_id': comment.review_id
+                } for comment in review_comments] 
+            }        
+            return JsonResponse({'status': "SUCCESS", 'data': {'course':course_info}}, status=200)
+        except Course.DoesNotExist:
+            return JsonResponse({"status": "COURSE_NOT_FOUND", "message": "존재하지 않는 클래스입니다."}, status=404)
